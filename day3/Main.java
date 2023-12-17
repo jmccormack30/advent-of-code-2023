db7dca16b2be4a77bd80a634e3d1420167248458
Jack McCormack <jack.mccormack1234@gmail.com>
2023-12-17 13:11:43 -0500

    Added the code for part 2 of the gear ratios problem

--- day3/Main.java
+++ day3/Main.java
@@ -6,7 +6,7 @@ class Main {
 		String fileName = "input.txt";
 
 		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-			int value = decodeFile_Part1(br);
+			int value = decodeFile_Part2(br);
 			System.out.println(value);
 		}
 		catch (FileNotFoundException fnfe) {
@@ -54,7 +54,7 @@ class Main {
 					if (foundNum) {
 						num = Integer.parseInt(number.toString());
 						end = (j == row.length - 1) ? j : (j - 1);
-						isValid = isValid(schematic, row, i, start - 1, end + 1);
+						isValid = isValid_Part1(schematic, row, i, start - 1, end + 1);
 					}
 
 					number = new StringBuilder();
@@ -105,77 +105,109 @@ class Main {
 		}
 		return false;
 	}
-}
 
-private static int decodeFile_Part2(BufferedReader br) throws Exception {
-	char[][] schematic = new char[140][140];
-	String line;
+	private static int decodeFile_Part2(BufferedReader br) throws Exception {
+		char[][] schematic = new char[140][140];
+		String line;
 
-	int i = 0;
-	while ((line = br.readLine()) != null) {
-		char[] arr = line.toCharArray();
-		schematic[i] = arr;
-		i++;
-	}
+		int i = 0;
+		while ((line = br.readLine()) != null) {
+			char[] arr = line.toCharArray();
+			schematic[i] = arr;
+			i++;
+		}
 
-	int sum = 0;
-	StringBuilder number = new StringBuilder();
-	boolean foundNum = false;
-	int start = -1;
-	int end = -1;
+		int sum = 0;
+		StringBuilder number = new StringBuilder();
+		boolean foundNum = false;
+		int start = -1;
+		int end = -1;
 
-	for (i = 0; i < schematic.length; i++) {
-		char[] row = schematic[i];
-	}
+		for (i = 0; i < schematic.length; i++) {
+			char[] row = schematic[i];
+		}
 
-	for (i = 0; i < schematic.length; i++) {
-		char[] row = schematic[i];
-		for (int j = 0; j < row.length; j++) {
-			char c = row[j];
+		for (i = 0; i < schematic.length; i++) {
+			char[] row = schematic[i];
+			for (int j = 0; j < row.length; j++) {
+				char c = row[j];
 
-			if ('*' == c) {
-				int product = calculateValue(schematic, row, i, j);
-				sum += product;
+				if ('*' == c) {
+					int product = calculateValue_Part2(schematic, row, i, j);
+					sum += product;
+				}
 			}
+		}
+
+		return sum;
+	}
+
+	private static int calculateValue_Part2(char[][] schematic, char[] row, int r, int j) {
+		int product = 1;
+		Map<String, Integer> numberLocationMap = new HashMap<>();
+
+		Map.Entry<String, Integer> leftNum = getNumber_Part2(row, r, j - 1, true, false);
+		Map.Entry<String, Integer> rightNum = getNumber_Part2(row, r, j + 1, false, true);
+		addToMap_Part2(numberLocationMap, leftNum);
+		addToMap_Part2(numberLocationMap, rightNum);
+
+		char[] above = (r - 1 >= 0) ? schematic[r - 1] : null;
+		Map.Entry<String, Integer> topLeftNum = getNumber_Part2(above, r - 1, j - 1, true, true);
+		Map.Entry<String, Integer> topMiddleNum = getNumber_Part2(above, r - 1, j, true, true);
+		Map.Entry<String, Integer> topRightNum = getNumber_Part2(above, r - 1, j + 1, true, true);
+		addToMap_Part2(numberLocationMap, topLeftNum);
+		addToMap_Part2(numberLocationMap, topMiddleNum);
+		addToMap_Part2(numberLocationMap, topRightNum);
+
+		char[] below = (r + 1 < schematic.length) ? schematic[r + 1] : null;
+		Map.Entry<String, Integer> bottomLeftNum = getNumber_Part2(below, r + 1, j - 1, true, true);
+		Map.Entry<String, Integer> bottomMiddleNum = getNumber_Part2(below, r + 1, j, true, true);
+		Map.Entry<String, Integer> bottomRightNum = getNumber_Part2(below, r + 1, j + 1, true, true);
+		addToMap_Part2(numberLocationMap, bottomLeftNum);
+		addToMap_Part2(numberLocationMap, bottomMiddleNum);
+		addToMap_Part2(numberLocationMap, bottomRightNum);
+
+		if (numberLocationMap.size() != 2) {
+			return 0;
+		}
+		else {
+			return getProduct_Part2(numberLocationMap);
 		}
 	}
 
-	return sum;
-}
+	private static Map.Entry<String, Integer> getNumber_Part2(char[] row, int r, int j, boolean searchLeft, boolean searchRight) {
+		StringBuilder number = new StringBuilder();
+		int firstIndex = j;
+		if (row == null || j < 0 || j >= row.length || !Character.isDigit(row[j])) {
+			return null;
+		}
+		number.append(String.valueOf(row[j]));
+		int i = j;
+		while (searchLeft && i - 1 >= 0 && Character.isDigit(row[i - 1])) {
+			number.insert(0, String.valueOf(row[i - 1]));
+			i--;
+			firstIndex = i;
+		}
+		i = j;
+		while (searchRight && i + 1 < row.length && Character.isDigit(row[i + 1])) {
+			number.append(String.valueOf(row[i + 1]));
+			i++;
+		}
+		String location = String.valueOf(r) + String.valueOf(firstIndex);
+		return new AbstractMap.SimpleImmutableEntry<>(location, Integer.parseInt(number.toString()));
+	}
 
-private static int calculateValue_Part2(char[][] schematic, char[] row, int r, int j) {
-	int totalNums = 0;
-	int product = 1;
-
-	int leftNum = getNumber(row, j - 1, true, false);
-	int rightNum = getNumber(row, j + 1, false, true);
-	if (leftNum != 0) {
-		product *= leftNum;
-		totalNums++;
-	}
-	if (rightNum != 0) {
-		product *= rightNum
-		totalNums++;
+	private static void addToMap_Part2(Map<String, Integer> numberLocationMap, Map.Entry<String, Integer> entry) {
+		if (entry != null) {
+			numberLocationMap.put(entry.getKey(), entry.getValue());
+		}
 	}
 
-	return product;
-}
-
-private static int getNumber_Part2(char[] row, int j, boolean searchLeft, boolean searchRight) {
-	StringBuilder number = new StringBuilder();
-	if (j < 0 || j >= row.length || !Character.isDigit(row[j])) {
-		return 0;
+	private static int getProduct_Part2(Map<String, Integer> numberLocationMap) {
+		int product = 1;
+		for (Map.Entry<String, Integer> entry : numberLocationMap.entrySet()) {
+			product *= entry.getValue();
+		}
+		return product;
 	}
-	number.append(String.valueOf(row[j]));
-	int i = j;
-	while (searchLeft && i - 1 >= 0 && Character.isDigit(row[i - 1])) {
-		number.insert(0, String.valueOf(row[i - 1]));
-		i--;
-	}
-	i = j;
-	while (searchRight && i + 1 < row.length && Character.isDigit(row[i + 1])) {
-		number.append(String.valueOf(row[i + 1]));
-		i++;
-	}
-	return Integer.parseInt(number.toString());
 }
