4c5b41981e8ce885351786852c7966cee8629ce3
Jack McCormack <jack.mccormack1234@gmail.com>
2023-12-19 14:26:08 -0500

    Completed part 2 using LCM approach

--- day8/Main.java
+++ day8/Main.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.math.*;
 
 class Main {
 	public static void main(String[] args) throws Exception {
@@ -7,7 +8,7 @@ class Main {
 
 		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
 			long startTime = System.nanoTime();
-			long value = decodeFile_Part1(br);
+			BigInteger value = decodeFile_Part2(br);
 			long endTime = System.nanoTime();
 			System.out.println("\n" + value);
 			System.out.println(getExecutionTimeString(startTime, endTime));
@@ -35,6 +36,24 @@ class Main {
 		Map<String, Node> nodeMap = createNodeMap(nodesStr);
 		updateNodes(nodeMap, nodesStr);
 		return calculateSteps(nodeMap, instructions, nodeMap.get("AAA"), nodeMap.get("ZZZ"));
+	}
+
+	private static BigInteger decodeFile_Part2(BufferedReader br) throws Exception {
+		StringBuilder builder = new StringBuilder();
+		String line;
+		while ((line = br.readLine()) != null) {
+			builder.append(line);
+			builder.append(System.lineSeparator());
+		}
+		String inputStr = builder.toString();
+		String[] arr = inputStr.split("\\n\\s*\\n");
+		String instructionsStr = arr[0].trim();
+		String nodesStr = arr[1];
+		char[] instructions = instructionsStr.toCharArray();
+		Map<String, Node> nodeMap = createNodeMap(nodesStr);
+		updateNodes(nodeMap, nodesStr);
+		Set<Node> startNodes = getStartNodes_Part2(nodeMap, 'A');
+		return calculateSteps_Part2(nodeMap, startNodes, instructions, 'Z');
 	}
 
 	private static Map<String, Node> createNodeMap(String nodesStr) {
@@ -74,6 +93,59 @@ class Main {
 		}
 		System.out.println(currentNode.getName());
 		return steps;
+	}
+
+	private static BigInteger calculateSteps_Part2(Map<String, Node> nodeMap, Set<Node> nodes, char[] instructions, char endChar) {
+		BigInteger[] leastStepsArr = new BigInteger[6];
+		Iterator<Node> iterator = nodes.iterator();
+		for (int i = 0; i < nodes.size(); i++) {
+			Node node = iterator.next();
+			BigInteger leastSteps = calculateLeastSteps(nodeMap, instructions, node, endChar);
+			leastStepsArr[i] = leastSteps;
+		}
+		return getLCM(leastStepsArr);
+	}
+
+	private static BigInteger calculateLeastSteps(Map<String, Node> nodeMap, char[] instructions, Node node, char endChar) {
+		BigInteger steps = new BigInteger("0");
+		int i = 0;
+		Node curNode = node;
+		while (endChar != curNode.getName().charAt(curNode.getName().length() - 1)) {
+			if (i == instructions.length) i = 0;
+			char direction = instructions[i];
+			curNode = ('L' == direction) ? curNode.getLeft() : curNode.getRight();
+			i += 1;
+			steps = steps.add(BigInteger.ONE);
+		}
+		return steps;
+	}
+
+	private static BigInteger getLCM(BigInteger[] arr) {
+		BigInteger b1 = findLCM(arr[0], arr[1]);
+		BigInteger b2 = findLCM(arr[2], arr[3]);
+		BigInteger b3 = findLCM(arr[4], arr[5]);
+
+		BigInteger b4 = findLCM(b1, b2);
+		return findLCM(b4, b3);
+	}
+
+	private static BigInteger findLCM(BigInteger a, BigInteger b) {
+		return a.multiply(b).divide(findGCD(a, b));
+	}
+
+	private static BigInteger findGCD(BigInteger a, BigInteger b) {
+		System.out.println(a + " | " + b);
+		return a.gcd(b);
+	}
+
+	private static Set<Node> getStartNodes_Part2(Map<String, Node> nodeMap, char character) {
+		Set<Node> startNodes = new HashSet<>();
+		for (Node node: nodeMap.values()) {
+			if (character == node.getName().charAt(node.getName().length() - 1)) {
+				startNodes.add(node);
+			}
+		}
+		return startNodes;
 	}
 
 	private static String getExecutionTimeString(long startTime, long endTime) {
